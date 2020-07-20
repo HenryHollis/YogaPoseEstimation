13c87cd26cd854303272261bbd841ebe1ba79572
Henry Hollis <41795183+HenryHollis@users.noreply.github.com>
2020-07-20 11:14:54 -0400

    Update yogaPoser.py

--- yogaPoser.py
+++ yogaPoser.py
@@ -1,7 +1,6 @@
 
 from __future__ import division
 import argparse, time, logging, os, math, tqdm, cv2
-from PIL import Image
 import numpy as np
 import mxnet as mx
 from mxnet import gluon, nd, image
@@ -18,19 +17,18 @@ import time
 import argparse
 import imutils
 from imutils.video import VideoStream
-import multiprocessing as mp
-from multiprocessing import Pool
 import os
 import pandas as pd
 
 ctx = mx.cpu()
 #detector_name = 'yolo3_mobilenet1.0_coco'
-detector_name = "ssd_512_mobilenet1.0_coco"
+detector_name = "ssd_512_mobilenet1.0_coco"       # network for detecting humans
+
 detector = get_model(detector_name, pretrained=True, ctx=ctx)
-detector.reset_class(classes=['person'], reuse_weights={'person':'person'})
+detector.reset_class(classes=['person'], reuse_weights={'person':'person'})  # only bother reporting class labeled "humans"
 detector.hybridize()
 
-estimator = get_model('simple_pose_resnet18_v1b', pretrained='ccd24037', ctx=ctx)
+estimator = get_model('simple_pose_resnet18_v1b', pretrained='ccd24037', ctx=ctx)  # model used for pose estimation
 estimator.hybridize()
 
 ap = argparse.ArgumentParser()
@@ -38,16 +36,15 @@ ap.add_argument('-v', '--vid', type = str, required = False, help ='video path')
 ap.add_argument('-o', '--outfile', default = 'output.avi', help = 'outfile path')
 args = vars(ap.parse_args())
 
-num_cores = mp.cpu_count()
 using_vid_file = False
 
-if args['vid'] is not None:
+if args['vid'] is not None:         # If getting frames from video
     using_vid_file = True
     print('[INFO] using video file...')
     vid_path = os.getcwd() + args['vid'][1:]
     vs = cv2.VideoCapture(vid_path)
     
-else:
+else:                              # If getting frames from webcam (default)
     print('[INFO] using camera...')
     vs = VideoStream(src=0).start()
     time.sleep(1.0)
@@ -56,23 +53,23 @@ else:
 #if using_vid_file:
 #    frame = frame[1]
 #(H, W) = frame.shape[:2]
-vid_writer = cv2.VideoWriter(args['outfile'],cv2.VideoWriter_fourcc(*'MJPG'), 29, ( 500, 280), True)
+vid_writer = cv2.VideoWriter(args['outfile'],cv2.VideoWriter_fourcc(*'MJPG'), 29, ( 500, 280), True)  # For outfile writing
 
 def main():
     start = time.time()
     count = 0
     pose = None
-    skip_frame = False
+    skip_frame = False     # to increase frame rate, I only process every other frame.
 
     #with open('data.txt', 'a') as outfile:
     
-    while True:
+    while True:    # While there is video frames to process...
 
         frame = vs.read()
         if using_vid_file:
             frame = frame[1]
         try:
-            frame = np.fliplr(frame)
+            frame = np.fliplr(frame)   # I want to display the mirror image if using webcam, personal choice
         except ValueError:
             print('[ERROR] video file not found, make sure to include path and extension i.e. \'./vid.mp4\'')
             break
@@ -107,7 +104,7 @@ def main():
 
         else:
             skip_frame = False
-        img = imutils.resize(img, height = 280, width = 500)
+        img = imutils.resize(img, height = 280, width = 500)    # blowup image for displaying
         vid_writer.write(img)
         if pose:
             cv2.putText(img, '{}'.format(pose), (20,20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
